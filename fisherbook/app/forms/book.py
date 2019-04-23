4eb72def18bd0bdb62ac97b760d47e031a085153
张张 <565393394@qq.com>
2019-04-23 10:20:26 +0800

    参数验证， 拆分配置文件

--- fisherbook/app/forms/book.py
+++ fisherbook/app/forms/book.py
@@ -2,11 +2,11 @@
 Created by 张 on 2019/4/22 
 """
 from wtforms import Form, StringField, IntegerField
-from wtforms.validators import Length, NumberRange
+from wtforms.validators import Length, NumberRange, DataRequired
 
 __author__ = '张'
 
 
 class SearchForm(Form):
-    q = StringField(validators=[Length(min=1, max=30)])
+    q = StringField(validators=[DataRequired(), Length(min=1, max=30)])
     page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
