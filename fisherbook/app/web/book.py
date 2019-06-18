b99e6955a978538a6b34785d6eb6c956bdc547ed
张张 <565393394@qq.com>
2019-06-19 00:11:58 +0800

    重构鱼书核心对象

--- fisherbook/app/web/book.py
+++ fisherbook/app/web/book.py
@@ -6,7 +6,7 @@ from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
 from app.forms.book import SearchForm
 from . import web
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookViewModel, BookCollection
 
 __author__ = '张'
 
@@ -15,17 +15,17 @@ __author__ = '张'
 @web.route('/book/search')
 def search():
     form = SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
         if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result, q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
-
-        return jsonify(result)
+            yushu_book.search_by_keyword(q,page)
+        books.fill(yushu_book, q)
+        return jsonify(books)
     else:
         return jsonify(form.errors)
