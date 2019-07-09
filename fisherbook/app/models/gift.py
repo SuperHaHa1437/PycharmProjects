894f6811307c30deaa29fc2c3de6065b59163f6a
张张 <565393394@qq.com>
2019-07-09 21:08:25 +0800

    可以显示赠送清单,但是不知为何没有显示人数,不知道怎么测试,用了其他账号去加心愿,但是还是没有显示出赠送清单的人数

--- fisherbook/app/models/gift.py
+++ fisherbook/app/models/gift.py
@@ -3,13 +3,17 @@ Created by 张 on 2019/6/30
 """
 from flask import current_app
 
+from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
 
 __author__ = '张'
 
 from app.models.base import db, Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey,desc
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, desc, func
 from sqlalchemy.orm import relationship
+from collections import namedtuple
+
+EachGiftWishCount = namedtuple('EachGiftWishCount', ['count', 'isbn'])
 
 
 class Gift(Base):
@@ -18,6 +22,22 @@ class Gift(Base):
     uid = Column(Integer, ForeignKey('user.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @classmethod
+    def get_user_gifts(cls, uid):
+        gifts = Gift.query.filter_by(uid=uid, launched=False).order_by(
+            desc(Gift.create_time)).all()
+        return gifts
+
+    @classmethod
+    def get_wish_counts(cls, isbn_list):
+        #     根据传入的一组 isbn,到 Wish 表中计算出某个礼物的 Wish 心愿数量
+        count_list = db.session.query(func.count(Wish.id), Wish.isbn).filter(
+            Wish.launched == False,
+            Wish.isbn.in_(isbn_list),
+            Wish.status == 1).group_by(Wish.isbn).all()
+        count_list = [{'count': w[0], 'isbn': w[1]} for w in count_list]
+        return count_list
 
     @property
     def book(self):
