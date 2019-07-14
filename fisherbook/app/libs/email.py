40c5f1a58f61804b4a62efb38d49ce1ffd36d9dc
张张 <565393394@qq.com>
2019-07-14 21:32:59 +0800

    异步发送电子邮件

--- fisherbook/app/libs/email.py
+++ fisherbook/app/libs/email.py
@@ -1,6 +1,8 @@
 """
 Created by 张 on 2019/7/13 
 """
+from threading import Thread
+
 from flask import current_app, render_template
 
 __author__ = '张'
@@ -8,10 +10,19 @@ __author__ = '张'
 from app import mail
 from flask_mail import Message
 
+def send_async_email(app,msg):
+    with app.app_context():
+        try:
+            mail.send(msg)
+        except Exception as e:
+            raise e
+
 
 def send_mail(to, subject, template, **kwargs):
     # msg = Message('鱼书测试邮件', sender='565393394@qq.com', body='Test',
     #               recipients=['565393394@qq.com'])
     msg = Message('[鱼书]' + ' ' + subject, sender=current_app.config['MAIL_USERNAME'], recipients=[to])
     msg.html = render_template(template, **kwargs)
-    mail.send(msg)
+    app = current_app._get_current_object()
+    thr = Thread(target=send_async_email,args=[app,msg])
+    thr.start()
