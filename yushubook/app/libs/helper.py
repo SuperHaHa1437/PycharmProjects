1cee429f7dd1f891b0a0b9cef526bb26be3ffa94
张张 <565393394@qq.com>
2019-08-05 11:47:36 +0800

    新建了鱼书项目

---
+++ yushubook/app/libs/helper.py
@@ -0,0 +1,21 @@
+"""
+Created by 张 on 2019/7/31 
+"""
+__author__ = '张'
+
+'''
+判断搜索字段是 isbn  还是关键字搜索
+'''
+
+
+def is_isbn_or_key(word):
+    isbn_or_key = 'key'
+
+    if len(word) == 13 and word.isdigit():
+        isbn_or_key = 'isbn'
+
+    short_word = word.replace('-', '')
+    if '-' in word and len(short_word) == 10 and short_word.isdigit():
+        isbn_or_key = 'isbn'
+
+    return isbn_or_key
